522cb37261c4ca63253039e2bbe1845c3ed92d82
Ayoubhmz <ayoub.hamzaoui@esprit.tn>
2024-01-29 20:42:21 +0100

    Adding JWT and Spring security dependency in pom.xml

--- src/main/java/com/example/jwt/controller/UserController.java
+++ src/main/java/com/example/jwt/controller/UserController.java
@@ -4,6 +4,7 @@ import com.example.jwt.entity.Role;
 import com.example.jwt.entity.User;
 import com.example.jwt.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -29,4 +30,14 @@ public class UserController {
         return userService.registerNewUser(user);
 
     }
+    @GetMapping({"/forAdmin"})
+    public String forAdmin(){
+        return "this URL is only accessible to admin";
+    }
+
+    @GetMapping({"/forUser"})
+    public String forUser(){
+        return "this URL is only accessible to the user";
+    }
+
 }
